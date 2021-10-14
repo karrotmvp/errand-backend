66da916fc2371afce1290388f57f5b76901cdadb
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-14 17:01:17 +0900

    feat: 비즈채팅 api

---
+++ src/main/kotlin/com/daangn/errand/rest/dto/daangn/PostBizChatReq.kt
@@ -0,0 +1,22 @@
+package com.daangn.errand.rest.dto.daangn
+
+data class PostBizChatReq(
+    val input: Input
+)
+
+data class Input(
+    val userId: String,
+    val title: String,
+    val text: String,
+    val actions: List<Action>,
+)
+
+data class Action(
+    val type: String,
+    val payload: Payload
+)
+
+data class Payload(
+    val linkUrl: String,
+    val text: String
+)
