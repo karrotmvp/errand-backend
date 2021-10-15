f85b6b9faddf5d3b4b294cd580598be4e3a2c0b5
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-15 11:54:57 +0900

    fix: category의 id가 AI가 아니었음
    AI로 고치고 데이터베이스 드랍 후 update 함

--- src/main/kotlin/com/daangn/errand/domain/HelperHasCategories.kt
+++ src/main/kotlin/com/daangn/errand/domain/HelperHasCategories.kt
@@ -1,6 +1,11 @@
 package com.daangn.errand.domain
 
+import au.com.console.kassava.kotlinEquals
+import au.com.console.kassava.kotlinHashCode
+import au.com.console.kassava.kotlinToString
+import com.daangn.errand.domain.category.Category
 import com.daangn.errand.domain.errand.Errand
+import com.daangn.errand.domain.help.Help
 import com.daangn.errand.domain.user.User
 import javax.persistence.*
 
@@ -12,4 +17,16 @@ class HelperHasCategories(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
+
+    companion object{
+        private val equalsAndHashCodeProperties = arrayOf(HelperHasCategories::id)
+        private val toStringProperties = arrayOf(
+            HelperHasCategories::id,
+            HelperHasCategories::helper
+        )
+    }
+
+    override fun toString(): String = kotlinToString(properties = toStringProperties)
+    override fun equals(other: Any?) = kotlinEquals(other = other, properties = equalsAndHashCodeProperties)
+    override fun hashCode() = kotlinHashCode(properties = equalsAndHashCodeProperties)
 }
--- src/main/kotlin/com/daangn/errand/domain/category/Category.kt
+++ src/main/kotlin/com/daangn/errand/domain/category/Category.kt
@@ -1,12 +1,29 @@
 package com.daangn.errand.domain.category
 
+import au.com.console.kassava.kotlinEquals
+import au.com.console.kassava.kotlinHashCode
+import au.com.console.kassava.kotlinToString
+import com.daangn.errand.domain.errand.Errand
 import javax.persistence.*
 
 @Entity
 class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
 
     @Column
     var name: String = ""
+
+    companion object{
+        private val equalsAndHashCodeProperties = arrayOf(Category::id)
+        private val toStringProperties = arrayOf(
+            Category::id,
+            Category::name
+        )
+    }
+
+    override fun toString(): String = kotlinToString(properties = toStringProperties)
+    override fun equals(other: Any?) = kotlinEquals(other = other, properties = equalsAndHashCodeProperties)
+    override fun hashCode() = kotlinHashCode(properties = equalsAndHashCodeProperties)
 }
