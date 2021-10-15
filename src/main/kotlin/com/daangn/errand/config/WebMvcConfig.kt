ae50298b4eb270f27d382a37cdcc44d73540c13c
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-15 11:31:24 +0900

    feat: tokenPayload argument resolver

--- src/main/kotlin/com/daangn/errand/config/WebMvcConfig.kt
+++ src/main/kotlin/com/daangn/errand/config/WebMvcConfig.kt
@@ -1,11 +1,19 @@
 package com.daangn.errand.config
 
+import com.daangn.errand.rest.resolver.TokenPayloadArgumentResolver
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebMvcConfig : WebMvcConfigurer {
+class WebMvcConfig(
+    val tokenPayloadArgumentResolver: TokenPayloadArgumentResolver
+) : WebMvcConfigurer {
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(tokenPayloadArgumentResolver)
+    }
+
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("swagger-ui.html")
     }
