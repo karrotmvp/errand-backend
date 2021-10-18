b462b7ce93171a68368244a256ad245e4b08364b
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-18 22:06:48 +0900

    feat: 나의 심부름 최신순으로 조회하는 dao 메서드

--- src/main/kotlin/com/daangn/errand/repository/ErrandQueryRepository.kt
+++ src/main/kotlin/com/daangn/errand/repository/ErrandQueryRepository.kt
@@ -1,8 +1,15 @@
 package com.daangn.errand.repository
 
 import com.daangn.errand.domain.errand.Errand
+import com.daangn.errand.domain.user.User
 
 interface ErrandQueryRepository {
     fun findErrandsAfterLastErrandOrderByCreatedAtDesc(lastErrand: Errand, size: Long): MutableList<Errand>
     fun findErrandOrderByCreatedAtDesc(size: Long): MutableList<Errand>
+    fun findByCustomerOrderByCreateAtDesc(customer: User, size: Long): MutableList<Errand>
+    fun findErrandsAfterLastErrandByCustomerOrderedByCreatedAtDesc(
+        lastErrand: Errand,
+        customer: User,
+        size: Long
+    ): MutableList<Errand>
 }
