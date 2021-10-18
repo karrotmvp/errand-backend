1f1b0bd1777f1110c336f469ed1373184f943295
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-18 20:55:28 +0900

    feat: errand query repository 추가

--- src/main/kotlin/com/daangn/errand/repository/ErrandRepository.kt
+++ src/main/kotlin/com/daangn/errand/repository/ErrandRepository.kt
@@ -3,5 +3,5 @@ package com.daangn.errand.repository
 import com.daangn.errand.domain.errand.Errand
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ErrandRepository: JpaRepository<Errand, Long> {
+interface ErrandRepository: JpaRepository<Errand, Long>, ErrandQueryRepository {
 }
