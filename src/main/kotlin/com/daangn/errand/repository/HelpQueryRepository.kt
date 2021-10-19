d222ae7225fd213d2b8c2f7f637447823c1ac38d
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-19 15:03:48 +0900

    feat: 내가 지원한 심부름 가져오기 DAO 메서드

---
+++ src/main/kotlin/com/daangn/errand/repository/HelpQueryRepository.kt
@@ -0,0 +1,9 @@
+package com.daangn.errand.repository
+
+import com.daangn.errand.domain.help.Help
+import com.daangn.errand.domain.user.User
+
+interface HelpQueryRepository {
+    fun findByHelperTopSize(helper: User, size: Long): MutableList<Help>
+    fun findByHelper(helper: User, lastHelp: Help, size: Long): MutableList<Help>
+}
--- src/main/kotlin/com/daangn/errand/repository/HelpRepository.kt
+++ src/main/kotlin/com/daangn/errand/repository/HelpRepository.kt
@@ -5,7 +5,7 @@ import com.daangn.errand.domain.help.Help
 import com.daangn.errand.domain.user.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface HelpRepository: JpaRepository<Help, Long> {
+interface HelpRepository: JpaRepository<Help, Long>, HelpQueryRepository {
     fun findByErrandAndHelper(errand: Errand, helper: User): Help?
     fun findByErrandOrderByCreatedAt(errand: Errand): MutableList<Help>
     fun countByErrand(errand: Errand): Long
