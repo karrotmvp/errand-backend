48f5f4184af376e3e66be03e09296013caf8a08d
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-19 14:57:37 +0900

    fix: Jpa auditing

--- src/main/kotlin/com/daangn/errand/ErrandApplication.kt
+++ src/main/kotlin/com/daangn/errand/ErrandApplication.kt
@@ -2,12 +2,11 @@ package com.daangn.errand
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-@RestController
-@RequestMapping("")
 class ErrandApplication
 
 fun main(args: Array<String>) {
