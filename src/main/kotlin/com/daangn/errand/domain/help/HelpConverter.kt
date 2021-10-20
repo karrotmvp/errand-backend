4d9da447fa41a1d0c36194c30232626d469db9ea
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-20 14:35:02 +0900

    fix: HelpVo는 심부름 ID만 갖도록 함

--- src/main/kotlin/com/daangn/errand/domain/help/HelpConverter.kt
+++ src/main/kotlin/com/daangn/errand/domain/help/HelpConverter.kt
@@ -3,10 +3,13 @@ package com.daangn.errand.domain.help
 import com.daangn.errand.domain.errand.ErrandConverter
 import com.daangn.errand.domain.user.UserConverter
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 import org.mapstruct.Mappings
 
-@Mapper(componentModel = "spring", uses = [UserConverter::class, ErrandConverter::class])
+@Mapper(componentModel = "spring", uses = [UserConverter::class])
 interface HelpConverter {
-    @Mappings
+    @Mappings(
+        Mapping(target = "errandId", source = "errand.id")
+    )
     fun toHelpVo(help: Help): HelpVo
 }
