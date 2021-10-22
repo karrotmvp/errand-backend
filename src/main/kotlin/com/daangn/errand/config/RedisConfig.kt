b70c67ce9e17bf441e4f92c76b7616c401367e19
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-22 20:50:32 +0900

    feat: redis util

---
+++ src/main/kotlin/com/daangn/errand/config/RedisConfig.kt
@@ -0,0 +1,23 @@
+package com.daangn.errand.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+@EnableRedisRepositories
+class RedisConfig {
+    @Bean
+    fun redisConnectionFactory() = LettuceConnectionFactory()
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any> {
+        val redisTemplate = RedisTemplate<String, Any>()
+        redisTemplate.setConnectionFactory(redisConnectionFactory())
+        redisTemplate.keySerializer = StringRedisSerializer()
+        return redisTemplate
+    }
+}
