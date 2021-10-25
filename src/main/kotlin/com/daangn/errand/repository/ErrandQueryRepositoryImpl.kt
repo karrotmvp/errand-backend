7d78fb31751c3413dcfce5e6f91cf8385c65b47e
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-25 21:37:24 +0900

    feat: 지원가능한 심부름을 가져오는 쿼리
    lastErrand가 있는 메서드, 없는 메서드를 구현

--- src/main/kotlin/com/daangn/errand/repository/ErrandQueryRepositoryImpl.kt
+++ src/main/kotlin/com/daangn/errand/repository/ErrandQueryRepositoryImpl.kt
@@ -2,13 +2,18 @@ package com.daangn.errand.repository
 
 import com.daangn.errand.domain.errand.Errand
 import com.daangn.errand.domain.errand.QErrand.errand
+import com.daangn.errand.domain.help.QHelp.help
 import com.daangn.errand.domain.user.User
 import com.querydsl.jpa.impl.JPAQueryFactory
 
 class ErrandQueryRepositoryImpl(
     val query: JPAQueryFactory
-): ErrandQueryRepository {
-    override fun findErrandsAfterLastErrandOrderByCreatedAtDesc(lastErrand: Errand, size: Long, regionIds: List<String>): MutableList<Errand> {
+) : ErrandQueryRepository {
+    override fun findErrandsAfterLastErrandOrderByCreatedAtDesc(
+        lastErrand: Errand,
+        size: Long,
+        regionIds: List<String>
+    ): MutableList<Errand> {
         return query.selectFrom(errand)
             .where(errand.regionId.`in`(regionIds))
             .where(errand.createdAt.before(lastErrand.createdAt))
@@ -40,6 +45,31 @@ class ErrandQueryRepositoryImpl(
     ): MutableList<Errand> {
         return query.selectFrom(errand)
             .where(errand.customer.eq(customer))
+            .where(errand.createdAt.before(lastErrand.createdAt))
+            .orderBy(errand.createdAt.desc())
+            .limit(size)
+            .fetch()
+    }
+
+    override fun findErrandsEnableToApply(size: Long, regionIds: List<String>): MutableList<Errand> {
+        return query.selectFrom(help).groupBy(help.errand)
+            .where(help.errand.count().lt(5))
+            .join(errand).on(help.errand.eq(errand))
+            .select(errand)
+            .orderBy(errand.createdAt.desc())
+            .limit(size)
+            .fetch()
+    }
+
+    override fun findErrandsEnableToApplyAfterLastErrand(
+        lastErrand: Errand,
+        size: Long,
+        regionIds: List<String>
+    ): MutableList<Errand> {
+        return query.selectFrom(help).groupBy(help.errand)
+            .where(help.errand.count().lt(5))
+            .join(errand).on(help.errand.eq(errand))
+            .select(errand)
             .where(errand.createdAt.before(lastErrand.createdAt))
             .orderBy(errand.createdAt.desc())
             .limit(size)
