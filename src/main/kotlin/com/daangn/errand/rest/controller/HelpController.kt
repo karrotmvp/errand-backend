9ca75722730d37315b9e0b0cb8abccafa46a8410
kayoung yoon <rkdkdudjd@gmail.com>
2021-10-28 19:35:48 +0900

    feat: 새로운 지원 이벤트 트래킹

--- src/main/kotlin/com/daangn/errand/rest/controller/HelpController.kt
+++ src/main/kotlin/com/daangn/errand/rest/controller/HelpController.kt
@@ -1,8 +1,11 @@
 package com.daangn.errand.rest.controller
 
+import com.daangn.errand.domain.help.HelpVo
 import com.daangn.errand.rest.dto.help.PostHelpReqDto
 import com.daangn.errand.rest.resolver.TokenPayload
 import com.daangn.errand.service.HelpService
+import com.daangn.errand.service.MixpanelService
+import com.daangn.errand.service.MixpanelTrackEvent
 import com.daangn.errand.support.response.ErrandResponse
 import com.daangn.errand.util.JwtPayload
 import org.springframework.http.HttpStatus
@@ -11,13 +14,20 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/help")
 class HelpController(
-    val helpService: HelpService
+    val helpService: HelpService,
+    val mixpanelService: MixpanelService
 ) {
     @PostMapping
     fun postHelp(
         @TokenPayload payload: JwtPayload,
         @RequestBody postHelpReqDto: PostHelpReqDto
-    ) = ErrandResponse(helpService.createHelp(payload.userId, postHelpReqDto))
+    ): ErrandResponse<HelpVo> {
+        // TODO: 이벤트마다 track event의 entity를 만드는 책임 분리
+        val entities = HashMap<String, String>()
+        entities.put("userId", payload.userId.toString())
+        mixpanelService.trackEvent(MixpanelTrackEvent.HELP_REGISTERED, entities) // TODO: 실제 이벤트로 리팩토링
+        return ErrandResponse(helpService.createHelp(payload.userId, postHelpReqDto))
+    }
 
     @DeleteMapping("/{id}")
     fun deleteHelp(
