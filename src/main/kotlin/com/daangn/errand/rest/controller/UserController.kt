951e355940f99cd05c20b53a3a5613633a00e895
kayoung yoon <rkdkdudjd@gmail.com>
2021-11-02 23:28:05 +0900

    feat: user category 알림 설정 api

--- src/main/kotlin/com/daangn/errand/rest/controller/UserController.kt
+++ src/main/kotlin/com/daangn/errand/rest/controller/UserController.kt
@@ -1,6 +1,7 @@
 package com.daangn.errand.rest.controller
 
 import com.daangn.errand.domain.user.UserProfileVo
+import com.daangn.errand.rest.dto.category.PatchUserCategoryReqDto
 import com.daangn.errand.rest.dto.patchUserAlarmReqDto
 import com.daangn.errand.rest.resolver.TokenPayload
 import com.daangn.errand.service.UserService
@@ -21,6 +22,17 @@ class UserController(
         @PathParam(value = "regionId") regionId: String
     ): ErrandResponse<UserProfileVo> {
         return ErrandResponse(userService.getUserWithDaangnProfile(payload.userId, payload.accessToken, regionId))
+    }
+
+    @PatchMapping("/category")
+    fun patchUserCategory(
+        @TokenPayload payload: JwtPayload,
+        @RequestBody patchUserCategoryReqDto: PatchUserCategoryReqDto
+    ): ErrandResponse<Any> {
+        if (patchUserCategoryReqDto.on)
+            userService.setCategory(payload.userId, patchUserCategoryReqDto.categoryId)
+        else userService.deactivateCategory(payload.userId, patchUserCategoryReqDto.categoryId)
+        return ErrandResponse(HttpStatus.OK)
     }
 
     @PatchMapping("/alarm")
