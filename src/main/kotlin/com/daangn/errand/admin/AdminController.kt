5933ab3afdd7d583cd0f8be269cdd87efc620a7e
kayoung yoon <rkdkdudjd@gmail.com>
2021-11-04 14:17:16 +0900

    feat: admin api

---
+++ src/main/kotlin/com/daangn/errand/admin/AdminController.kt
@@ -0,0 +1,57 @@
+package com.daangn.errand.admin
+
+import com.daangn.errand.domain.errand.ErrandConverter
+import com.daangn.errand.repository.ErrandRepository
+import com.daangn.errand.service.AuthService
+import com.daangn.errand.service.ErrandService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+@RequestMapping("/admin")
+class AdminController(
+    private val errandRepository: ErrandRepository,
+    private val errandConverter: ErrandConverter,
+    private val adminService: AdminService,
+) {
+    @GetMapping("")
+    fun getErrandList(model: Model): String {
+        val errands = errandRepository.findAll().map { errand -> errandConverter.toErrandDto(errand) }
+        model.addAttribute("errandList", errands)
+        return "errand-list"
+    }
+
+    @GetMapping("/errand/{id}")
+    fun getErrandDetail(model: Model, @PathVariable(value = "id") id: Long): String {
+        val errand = errandRepository.findById(id)
+        if (errand.isEmpty) return "error"
+        val errandDto = errandConverter.toErrandDto(errand.get())
+        model.addAttribute("errand", errandDto)
+        model.addAttribute("unexposed", errand.get().unexposed)
+        return "errand-detail"
+    }
+
+    @GetMapping("/errand/{id}/unexposed")
+    @ResponseBody
+    fun makeErrandUnexposed(
+        @PathVariable(value = "id") id: Long,
+    ): ResponseEntity<String> {
+        adminService.makeUnexposed(id)
+        return ResponseEntity<String>(true.toString(), HttpStatus.OK)
+    }
+
+    @GetMapping("/errand/{id}/exposed")
+    @ResponseBody
+    fun makeErrandExposed(
+        @PathVariable(value = "id") id: Long,
+    ): ResponseEntity<Boolean> {
+        adminService.makeExposed(id)
+        return ResponseEntity<Boolean>(false, HttpStatus.OK)
+    }
+}
