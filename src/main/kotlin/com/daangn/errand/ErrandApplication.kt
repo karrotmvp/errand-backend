794268a08fb19799b51d06b2dee7a2e4d44db55b
kayoung yoon <rkdkdudjd@gmail.com>
2021-11-16 18:36:40 +0900

    fix: query projection arguments

--- src/main/kotlin/com/daangn/errand/ErrandApplication.kt
+++ src/main/kotlin/com/daangn/errand/ErrandApplication.kt
@@ -2,11 +2,8 @@ package com.daangn.errand
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 @EnableScheduling
