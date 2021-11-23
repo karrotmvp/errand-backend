e5390d4cb582a0fd990799dd7a6d9712f009d8ea
kayoung yoon <rkdkdudjd@gmail.com>
2021-11-23 13:01:10 +0900

    feat: 알림 전송 전 완료 확정시 대기 중인 알림 이벤트 삭제

--- src/main/kotlin/com/daangn/errand/repository/CompleteNotiEventRepository.kt
+++ src/main/kotlin/com/daangn/errand/repository/CompleteNotiEventRepository.kt
@@ -1,9 +1,11 @@
 package com.daangn.errand.repository
 
+import com.daangn.errand.domain.errand.Errand
 import com.daangn.errand.domain.event.CompleteNotiEvent
 import org.springframework.data.jpa.repository.JpaRepository
 import java.time.LocalDateTime
 
 interface CompleteNotiEventRepository : JpaRepository<CompleteNotiEvent, Long> {
     fun findAllByScheduledAtBeforeAndHandledAtIsNullOrderByScheduledAt(scheduledAt: LocalDateTime): MutableList<CompleteNotiEvent>
+    fun findByErrand(errand: Errand): CompleteNotiEvent?
 }
